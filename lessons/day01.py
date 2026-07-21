716aacb694d95c081fec8f36ed286f0bad4c3d72
aadewole <adewole619@gmail.com>
2026-07-21 14:55:38 +0100

    Week 1 Day 1: Learned print statements and comments

---
+++ lessons/day01.py
@@ -0,0 +1,1 @@
+print("Hello, Receipt App!")
---
+++ lessons/practice_day01.py
@@ -0,0 +1,24 @@
+# Introduction to Python Programming with Receipt-App
+print("Practice Day 1: Hello, Receipt App!")
+
+# My name is Ayoola Adewole. I am a beginner in Python programming and I am excited to learn more about it. In this practice session, I will be exploring the basics of Python programming and how to use it to create a simple receipt application.
+print("By Ayoola Adewole")
+
+# my favorite food is Jollof Rice. It is a popular West African dish made with rice, tomatoes, onions, and a variety of spices. I enjoy cooking and eating Jollof Rice, and it is one of my favorite comfort foods.
+print("My favorite food is Jollof Rice.")
+
+# my dream job is to become a Software Engineer. I am passionate about technology and programming, and I aspire to work in the field of software development. I am eager to learn new programming languages and frameworks, and I hope to contribute to innovative software solutions in the future.
+print("My dream job is to become a Software Engineer.")
+
+# Welcome message for the receipt app
+print("Welcome to My Receipt App.")
+
+print("******************************")
+print("        Hello, Receipt App!   ")
+print("******************************")
+
+print("Welcome!\nThis program will help you save receipts.\nLet's begin...")
+
+Python
+
+Receipt App
